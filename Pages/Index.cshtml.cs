c147ca32ec64bd2aad1b7eb7acd867532874bf35
Ankur Sheel <ankur.sheel@gmail.com>
2020-06-23 08:47:15 +1200

    add form

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XSRF_test
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "Enter your message here";
+        }
+
+        public void OnPost()
+        {
+            Message = Request.Form[nameof(Message)];
+        }
+    }
+}
